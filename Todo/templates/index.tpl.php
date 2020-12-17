11b4e2513baae26a93baf82654d56d8e16c7cb0b
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2020-12-17 21:04:40 +0100

    Update index.tpl.php

--- Todo/templates/index.tpl.php
+++ Todo/templates/index.tpl.php
@@ -5,7 +5,7 @@
 <title>Proyecte APP</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/public/style.css">
+  <link rel="stylesheet" href="public/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
  
  </head>
